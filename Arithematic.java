35c065ca342177896a8dbe20cbe1e95f0b7be7a5
Ruchi141105 <163149323+Ruchi141105@users.noreply.github.com>
2024-09-10 20:20:46 +0530

    Add files via upload

---
+++ Arithematic.java
@@ -0,0 +1,13 @@
+
+public class Arithematic {
+    public static void main(String[] args) {
+        int num1=5, num2=10;
+        System.out.println("num1="+num1+"  num2="+num2);
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+        
+    }
+}
